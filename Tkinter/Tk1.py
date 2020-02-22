ed3f4c620625e980b1c74c6b829a6a4e36163e9b
İbrahim Ediz <Kurs>
2020-02-22 16:48:21 +0200

    22022020

--- Tkinter/Tk1.py
+++ Tkinter/Tk1.py
@@ -1,3 +1,24 @@
-import tkinter as tk
-pencere = tk.Tk()
+from tkinter import *
+pencere = Tk()
+#-------------------
+etiket = Label(text="Adı:")
+etiket.grid(column=0,row=0)
+#--------------------
+textBox = Entry()
+textBox.grid(column=1,row=0)
+#-------------------
+def tiklandi():
+    print(textBox.get())
+    print(rdb.getboolean(varRdb))
+    print(varRdb)
+dugme = Button(text="Aktarım",command=tiklandi)
+dugme.grid(column=2,row=0)
+#--------------------
+varRdb = BooleanVar()
+varRdb = False
+rdb = Radiobutton(text="deneme",value=varRdb,command=tiklandi)
+rdb.grid(column=3,row=0)
+#--------------------
+pencere.geometry("300x300+100+100")
+pencere.title("Kamil")
 pencere.mainloop()
